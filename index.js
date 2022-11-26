5363c6df4d9c24cddfd1eb1ae8ed253e4c3b8238
icika-max <icika-max>
2022-11-26 11:37:31 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,23 @@
+// grade - funtion that generates students grade
+// marks -parameters
+//return - student grades depending on the marks they got
+
+function grade (marks){//Grade A
+    if (marks>79){
+    return "A";
+ }
+ else if (marks <=79 && marks >=60 )//Grade B
+ {
+return "B"
+
+ }
+ else if (marks<=59 && marks>=50){ //Grade C
+    return "C"
+ }
+ else if (marks<=49 && marks>=40){//Grade D
+    return "D"
+ }
+else{//Grade E (0-39)
+    return "E"
+}
+}
